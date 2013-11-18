79591a63a7583f4ee7aa6917ccb3497687423bfe
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-18 13:20:56 -0600

    add rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShotener::Application.config.secret_key_base = '2d8086ebed9725bbe2445fa8211054b839079b894581a4d86b55678a2f3065f41d1ddbfde239d079d1b190eb364261e44e8350bdd503f022b9344b1cef0461e9'
